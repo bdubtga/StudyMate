35e8b607ec5ef59a725c06459a56998e2a35403d
Benji Wallis <benjiwallis2008@gmail.com>
2023-07-19 09:57:49 +1200

    Initial Commit

---
+++ StudyMate/StudyMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyMateApp.swift
+//  StudyMate
+//
+//  Created by Benji Wallis on 19/07/23.
+//
+
+import SwiftUI
+
+@main
+struct StudyMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
